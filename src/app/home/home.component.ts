8b4e8cacad6fdb717233b6909239eb665cacf7b4
Joseph Alcantara <joseph.alcantara@komsatsolutions.onmicrosoft.com>
2024-06-04 02:28:27 +0800

    add home

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent implements OnInit{
+  fullName: string = 'Joseph Alcantara';
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+}
