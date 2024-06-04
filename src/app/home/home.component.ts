eb3aa16f3b9d5434754cfdf0cf29b58807bb4690
Joseph Alcantara <joseph.alcantara@komsatsolutions.onmicrosoft.com>
2024-06-04 16:57:26 +0800

    Add experience

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,28 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { Experience } from '../interface/experience';
 import { CommonModule } from '@angular/common';
+import experiences from "../../assets/experiences.json";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit{
   fullName: string = 'Joseph Alcantara';
+  experienceData: Experience[] = experiences.experiences;
+
 
   constructor() {}
 
   ngOnInit(): void {
   }
-
-  experience1: Experience = {
-    id: 0,
-    date: '2024 — present',
-    position: 'Software Engineer',
-    company: 'Isentia',
-    description: 'test desc',
-    skills: ['javascript', 'c#']
-  };
 }
